ff0136c7554a5bfeef255c8a6c4b5ff925fe8928
Amarjit Singh <b.singh.amarjit@gmail.com>
2018-12-05 14:57:10 -0800

    add item button

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,18 +10,17 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["Find Mike","Buy Eggs","Destory Demogorgon"]
+    var itemArray = ["Find Mike","Buy Eggs","Destory Demogorgon"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        
     }
 
-    //Mark: Tableview Datascoure Methods
+    //MARK: Tableview Datascoure Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return itemArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -33,7 +32,7 @@ class TodoListViewController: UITableViewController {
     }
 
     
-    //Mark: TableView Delegate Methods
+    //MARK: TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
@@ -46,5 +45,30 @@ class TodoListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
         
     }
+    
+    //MARK: Add New Items
+    @IBAction func addButtonPressed(_ sender: Any) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todo Item", message: "", preferredStyle: .alert)
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Add New Item"
+            textField = alertTextField
+        }
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            if (textField.text != ""){ self.itemArray.append(textField.text!) }
+            self.tableView.reloadData()
+        }
+        
+        alert.addAction(action)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
 
