f15ce8939b59504dff3f19f958db492ab14ae673
Amarjit Singh <b.singh.amarjit@gmail.com>
2018-12-05 15:04:58 -0800

    add Presist data code

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -12,10 +12,15 @@ class TodoListViewController: UITableViewController {
 
     var itemArray = ["Find Mike","Buy Eggs","Destory Demogorgon"]
     
+    let userDeafults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        if let items = userDeafults.array(forKey: "TodoListArray") as? [String]{
+            itemArray = items
+        }
     }
 
     //MARK: Tableview Datascoure Methods
@@ -60,6 +65,8 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             if (textField.text != ""){ self.itemArray.append(textField.text!) }
+            
+            self.userDeafults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()
         }
         
