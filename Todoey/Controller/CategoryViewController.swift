8d71e77433ac1d0d38906e5f17f2f75bc842cbba
Amarjit Singh <b.singh.amarjit@gmail.com>
2018-12-05 21:50:33 -0800

    update pods

--- Todoey/Controller/CategoryViewController.swift
+++ Todoey/Controller/CategoryViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RealmSwift
+import ChameleonFramework
 
 class CategoryViewController: SwipeTableViewController {
     
@@ -33,6 +34,8 @@ class CategoryViewController: SwipeTableViewController {
         let cell = super.tableView(tableView, cellForRowAt: indexPath)
         
         cell.textLabel?.text = categories?[indexPath.row].name ?? "No Categories Added Yet"
+        
+        cell.backgroundColor = UIColor.randomFlat
         
         return cell
     }
