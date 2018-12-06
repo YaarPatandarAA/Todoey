55c149a0f00360e56313823a33ed9c50158cdd2e
Amarjit Singh <b.singh.amarjit@gmail.com>
2018-12-05 17:27:11 -0800

    rearange tablesView
    add new table view

---
+++ Todoey/Controller/CategoryViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Amarjit Singh on 12/5/18.
+//  Copyright © 2018 Amarjit Singh. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+}
